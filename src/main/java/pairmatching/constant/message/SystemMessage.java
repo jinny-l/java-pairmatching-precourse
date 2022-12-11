9ed9ee3c7b16635d24b1c1aa3d98c26c07ccc9fd
jinny-l <jinny.l.dev@gmail.com>
2022-12-11 15:49:14 +0900

    feat: 과정, 레벨, 미션을 입력하는 기능 구현

--- src/main/java/pairmatching/constant/message/SystemMessage.java
+++ src/main/java/pairmatching/constant/message/SystemMessage.java
@@ -1,8 +1,16 @@
 package pairmatching.constant.message;
 
+import pairmatching.constant.information.Course;
+import pairmatching.constant.information.Mission;
+
 public enum SystemMessage {
 
     INPUT_COMMAND("기능을 선택하세요."),
+    INPUT_INFORMATION("과정, 레벨, 미션을 선택하세요."),
+    INPUT_INFORMATION_EXAMPLE("ex) " + String.join(",",
+            Course.values()[0].getName(),
+            Mission.values()[0].getLevel(),
+            Mission.values()[0].getMissions().get(0))),
     OUTPUT_COURSE("과정: "),
     OUTPUT_MISSION("미션: ");
 
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -1,16 +1,28 @@
 package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import pairmatching.constant.Command;
 import pairmatching.constant.message.ErrorMessage;
 import pairmatching.constant.message.SystemMessage;
 
 public class InputView {
 
+    private static final String INFORMATION_DELIMITER = ", ";
+
     public static Command readCommand() {
         System.out.println(SystemMessage.INPUT_COMMAND);
         OutputView.printCommand();
         return Command.from(readLine());
+    }
+
+    public static List<String> readInformation() {
+        System.out.println(SystemMessage.INPUT_INFORMATION);
+        System.out.println(SystemMessage.INPUT_INFORMATION_EXAMPLE);
+        return Stream.of(readLine().split("\\s*" + INFORMATION_DELIMITER + "\\s*"))
+                .collect(Collectors.toList());
     }
 
     private static String readLine() {
