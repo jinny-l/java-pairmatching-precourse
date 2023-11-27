ffa5eca20d870202117b901d9e65be1bd5e29a48
jinny-l <jinny.l.dev@gmail.com>
2023-11-27 16:44:45 +0900

    feat: 매칭 정보 입력 기능 구현

---
+++ src/main/java/pairmatching/domain/MatchingInfo.java
@@ -0,0 +1,19 @@
+package pairmatching.domain;
+
+public class MatchingInfo {
+
+    private final Course course;
+    private final Mission mission;
+
+    private MatchingInfo(Course course, Mission mission) {
+        this.course = course;
+        this.mission = mission;
+    }
+
+    public static MatchingInfo from(String courseName, String levelName, String missionName) {
+        return new MatchingInfo(
+                Course.from(courseName),
+                Mission.from(missionName, levelName)
+        );
+    }
+}
--- src/main/java/pairmatching/view/InputView.java
+++ src/main/java/pairmatching/view/InputView.java
@@ -2,6 +2,7 @@ package pairmatching.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import pairmatching.domain.Command;
+import pairmatching.domain.MatchingInfo;
 
 public class InputView {
 
@@ -10,6 +11,18 @@ public class InputView {
 
     public static Command readCommand() {
         return Command.from(readLine());
+    }
+
+    public static MatchingInfo readMatchingInfo() {
+        System.out.printf("과정, 레벨, 미션을 선택하세요.%nex) 백엔드, 레벨1, 자동차경주%n");
+
+        String[] input = readLine().split(", "); // TODO 유효성 검증
+
+        return MatchingInfo.from(
+                input[0],
+                input[1],
+                input[2]
+        );
     }
 
     private static String readLine() {
